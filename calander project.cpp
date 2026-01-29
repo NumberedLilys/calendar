eeea0ebc8cd253ec59a2236abc511a0bd889447e
Lily Hamilton <lily.hamilton@ontariotechu.net>
2026-01-29 13:09:09 -0500

    number initialization

--- calander project.cpp
+++ calander project.cpp
@@ -4,6 +4,9 @@ int main() {
     std::cout << "Hello World" << std::endl;
 
     std::cout << "This is a calendar project." << std::endl;
+
+    int number = 5;
+    std::cout << "The number is: " << number << std::endl;
     
     return 0;
 }
