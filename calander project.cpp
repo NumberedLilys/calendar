86feeea26043353925fef3be944c1fde6edb393c
Lily Hamilton <lily.hamilton@ontariotechu.net>
2026-01-29 13:41:24 -0500

    Removed previous unnecessary changes

--- calander project.cpp
+++ calander project.cpp
@@ -2,11 +2,6 @@
 
 int main() {
     std::cout << "Hello World" << std::endl;
-
-    std::cout << "This is a calendar project." << std::endl;
-
-    int number = 5;
-    std::cout << "The number is: " << number << std::endl;
     
     return 0;
 }
